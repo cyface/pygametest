5dbc6976a8e54471577001442048b913d1b235f9
Tim White <twhite@fool.com>
2019-04-27 10:51:05 -0600

    Update soundy.py

--- soundy.py
+++ soundy.py
@@ -1,16 +1,14 @@
-import pygame
-import time
-import gpiozero
+from pygame import mixer
+from time import sleep
+from gpiozero import Button
 
-# pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
-pygame.mixer.init()
+mixer.init()
 
-soundObj = pygame.mixer.Sound('laser.wav')
+soundObj = mixer.Sound('laser.wav')
 soundObj.play()
 
-button = gpiozero.Button(23)
+button = Button(23)
 while True:
     if button.is_pressed:
-        print("Pressed")
         soundObj.play()
-        time.sleep(.25)  # wait and let the sound play for X second
+        sleep(.25)  # wait and let the sound play for X second
